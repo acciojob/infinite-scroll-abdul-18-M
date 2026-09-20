bcf0c4f2ea4ce2a840bfcee2cbe0c213b9838c57
Abdul Mulla <abdulmulla8071@gmail.com>
2026-09-20 23:04:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
+const lists = document.querySelector("#infi-list");
 
+let count = 1
+
+function addItems(){
+for(let i = 0;i<10;i++){
+	const li = document.createElement("li");
+	li.innerHTML = `List Item ${count}`
+	lists.appendChild(li);
+	count++;
+}
+}
+addItems(10);
+
+window.addEventListener("scroll",()=>{
+	if(window.innerHeight + window.scrollY >= document.body.offsetHeight){
+		addItems(2)
+	};
+})
