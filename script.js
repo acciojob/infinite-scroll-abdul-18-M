7f5284744dd56609fc9f9d610964df90bc88d7ad
Abdul Mulla <abdulmulla8071@gmail.com>
2026-09-25 17:21:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const lists = document.querySelector("#infi-list");
 
 let count = 1
 
-function addItems(num){
+function addItems(num = 2){
 for(let i = 0;i<num;i++){
 	const li = document.createElement("li");
 	li.innerHTML = `List Item ${count}`
@@ -13,8 +13,14 @@ for(let i = 0;i<num;i++){
 }
 addItems(10);
 
-window.addEventListener("scroll",()=>{
-	if(window.innerHeight + window.scrollY >= document.body.offsetHeight){
-		addItems(2)
-	};
-})
+lists.addEventListener("scroll", () => {
+    if (lists.scrollTop + lists.clientHeight >= lists.scrollHeight) {
+        addItems();
+    }
+});
+
+window.addEventListener("scroll", () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        addItems();
+    }
+});
